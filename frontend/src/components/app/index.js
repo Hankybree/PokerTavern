b7a9cb14d17119c6da6ce439749d0c31e294a34b
Henrik Frank <frank.havsvinge@gmail.com>
2020-11-21 19:57:50 +0100

    Created inital design and replaced logo

--- frontend/src/components/app/index.js
+++ frontend/src/components/app/index.js
@@ -1,25 +1,20 @@
-import logo from '../../assets/images/logo.svg';
+import logo from '../../assets/images/logo.png';
 import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <div id="content">
+        <img id="logo" src={logo} alt="Logo"/>
+        <h1 id="headline">TavernPoker</h1>
+        <form id="login-form">
+        <input type="text" placeholder="Username..."/>
+        <input type="password" placeholder="Password..."/>
+        <input type="button" value="login"/>
+      </form>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
