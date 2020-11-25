b27cb27668e7b9f4ba5ab2b08023a6214bb2ef23
Henrik Frank <frank.havsvinge@gmail.com>
2020-11-25 17:38:52 +0100

    Menu component fetches active tables and renders them into list

--- frontend/src/components/menu/index.js
+++ frontend/src/components/menu/index.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { useEffect, useState } from 'react'
 
 const Content = styled.div`
   display: flex;
@@ -9,13 +10,41 @@ const Content = styled.div`
 `
 
 function Menu() {
+  const [tables, setTables] = useState(null)
+
+  useEffect(() => {
+    getTables()
+      .then((tables) => {
+        setTables(tables)
+      })
+  }, [])
+
   return (
     <Content>
       <h1>Available tables</h1>
-
+      {tables && tables.map(table => <div key={table.tableId}>{table.tableName + ' ' + table.tableActivePlayers + '/' + table.tableMaxPlayers}</div>)}
       <input type="button" value="Log out" onClick={logout} />
     </Content>
   )
+}
+
+function getTables() {
+  return new Promise((resolve) => {
+    fetch('http://localhost:4000/game/tables', {
+      headers: {
+        'authorization': localStorage.getItem('token')
+      },
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log(result.tables)
+        resolve(result.tables)
+      }).catch(err => {
+        alert(err)
+        resolve(null)
+      })
+  })
 }
 
 function logout() {
